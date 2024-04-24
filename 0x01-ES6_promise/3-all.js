e7746d4b3603851a5ab8b5180413b4e30ec21354
piccolojnr <rahimdaud246@gmail.com>
2024-04-24 07:49:33 +0000

    finished

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((res) => console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`))
-    .catch(() => new Error('Signup system offline'));
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
