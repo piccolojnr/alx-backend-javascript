55fac2d8ca8e96c76fa2631e633d6f9e21ff5b25
piccolojnr <rahimdaud246@gmail.com>
2024-04-30 08:16:45 +0000

    eslint

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,15 @@
 export default function cleanSet(set, startString) {
   if (
-    !set ||
-    !startString ||
-    !(set instanceof Set) ||
-    typeof startString !== "string"
+    !set
+    || !startString
+    || !(set instanceof Set)
+    || typeof startString !== 'string'
   ) {
-    return "";
+    return '';
   }
 
   return Array.from(set)
-    .filter((item) => typeof item === "string" && item.startsWith(startString))
+    .filter((item) => typeof item === 'string' && item.startsWith(startString))
     .map((item) => item.slice(startString.length))
-    .join("-");
+    .join('-');
 }
