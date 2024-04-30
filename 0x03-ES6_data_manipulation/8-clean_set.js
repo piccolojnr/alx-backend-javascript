c856ac89b647124f6853c307f39830966234fb50
piccolojnr <rahimdaud246@gmail.com>
2024-04-30 08:16:16 +0000

    clean

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,15 @@
-export default function cleanSet(s, startString) {
-  return Array.from(s)
-    .filter((item) => (startString ? item.startsWith(startString) : false))
+export default function cleanSet(set, startString) {
+  if (
+    !set ||
+    !startString ||
+    !(set instanceof Set) ||
+    typeof startString !== "string"
+  ) {
+    return "";
+  }
+
+  return Array.from(set)
+    .filter((item) => typeof item === "string" && item.startsWith(startString))
     .map((item) => item.slice(startString.length))
-    .join('-');
+    .join("-");
 }
