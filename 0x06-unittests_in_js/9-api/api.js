4e32cbea402d344abed1a9fd33a6da60b16768c3
piccolojnr <rahimdaud246@gmail.com>
2024-06-20 14:20:54 +0000

    feat: Add API server with basic routes and tests

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 7865;
+
+app.use((req, res, next) => {
+  console.log("API available on localhost port 7865");
+  next();
+});
+
+// Route for GET /
+app.get("/", (req, res) => {
+  res.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id([0-9]+)", (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,22 @@
+const request = require("request");
+const { expect } = require("chai/index.js");
+
+describe("Index page", () => {
+  const API_URL = "http://localhost:7865";
+
+  it("GET / returns correct response", (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal("Welcome to the payment system");
+      done();
+    });
+  });
+
+  it("GET /cart/12 returns correct response", (done) => {
+    request.get(`${API_URL}/cart/12`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal("Payment methods for cart 12");
+      done();
+    });
+  });
+});
